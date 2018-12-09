2e559a75a860260db5b3b444b3616ffd914b2248
Daniel schwartz <mdblakestj@gmail.com>
2018-12-09 09:47:13 -0500

    Added React Router and routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp.js'
+import { BrowserRouter, Route} from 'react-router-dom';
+// import IndecisionApp from './components/IndecisionApp.js;
+//import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
-ReactDOM.render(<IndecisionApp/>, document.getElementById('app'))
+const ExpenseDashboardPage = () => (
+  <div>
+      This is from my dashboard component
+  </div>
+)
+const AddExpensePage = () => (
+  <div>
+      We are at Add
+  </div>
+)
+const EditExpensePage = () => (
+  <div>
+      We are at Edit
+  </div>
+)
+const HelpPage = () => (
+  <div>
+      We are at help
+  </div>
+)
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+      <Route path="/create" component={AddExpensePage}/>
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage}/>
+    </div>
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'))
--- src/playground/ES6letconst.js
+++
@@ -1,3 +0,0 @@
-var nameVar ='Andrew';
-
-console.log('nameVar')
